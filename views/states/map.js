280afa65c54d1129b8ff8d282d79b3d0cd591899
Simon Metson <simonmetson@googlemail.com>
2011-04-01 17:05:37 +0100

    Build states entirely from documents, colours and all

--- views/states/map.js
+++ views/states/map.js
@@ -1,5 +1,5 @@
 function(doc) {
   if (doc.state_name){
-     emit(doc.state_position, doc.state_shortcut + ',' + doc.state_name);
+     emit(doc.state_position, {shortcut:doc.state_shortcut, name:doc.state_name, colours: doc.state_colours});
   }
 }
