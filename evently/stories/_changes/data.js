b022f0c6813dd203f913d67dd82d457522fdc4c1
Mike Wallace <mikewallace1979@googlemail.com>
2011-04-04 14:00:29 +0100

    Save state to CouchDB when moving stories

--- evently/stories/_changes/data.js
+++ evently/stories/_changes/data.js
@@ -1,4 +1,17 @@
 function(data) {
+  $$(this).states_by_id = get_current_states(data);
   return_data = {stories: data.rows};
   return return_data;
+}
+
+var get_current_states = function(data) {
+  var states_by_id = {};
+  for (var i in data.rows) {
+    if (data.rows.hasOwnProperty(i)) {
+      id = data.rows[i];
+      state = data.rows[i].value.split(',')[1];
+      states_by_id[id] = state;
+    }
+  }
+  return states_by_id;
 }
