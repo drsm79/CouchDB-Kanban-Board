10b63785dbc23f172329f4b666c3c556eaff57fc
Simon Metson <simonmetson@googlemail.com>
2011-04-13 21:54:46 +0100

    Manage completed stories

--- views/stories/map.js
+++ views/stories/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.story_state){
+  if (doc.story_state && doc.story_state != 'Archived'){
      emit(doc.story_target, doc.story_state + ',' + doc._id + ',' + doc.story_name);
   }
 }
